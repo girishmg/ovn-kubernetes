girishmg/ovn-kubernetes

commit a9cd71c62721cb930d1bbdb56f1caaad3fd7efe6

SDN-194 Cannot access K8s service cluster IP from the K8s master
Signed-off-by: Yun Zhou <yunz@nvidia.com>
Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/cluster/master.go
@@ -323,7 +323,7 @@ func (cluster *OvnClusterController) deleteNode(nodeName string, nodeSubnet *net
 	}
 
 	if nodeSubnet != nil {
-		if err := util.GatewayCleanup(nodeName, nodeSubnet.String()); err != nil {
+		if err := util.GatewayCleanup(nodeName); err != nil {
 			return fmt.Errorf("Failed to clean up node %s gateway: (%v)", nodeName, err)
 		}
 	}

go-controller/pkg/cluster/gateway_init.go
@@ -15,7 +15,7 @@ import (
 // bridgedGatewayNodeSetup makes the bridge's MAC address permanent, sets up
 // the physical network name mappings for the bridge, and returns an ifaceID
 // created from the bridge name and the node name
-func bridgedGatewayNodeSetup(nodeName, bridgeInterface string) (string, string, error) {
+func bridgedGatewayNodeSetup(nodeName, bridgeInterface string, physicalNetworkName string) (string, string, error) {
 	// A OVS bridge's mac address can change when ports are added to it.
 	// We cannot let that happen, so make the bridge mac address permanent.
 	macAddress, err := util.GetOVSPortMACAddress(bridgeInterface)
@@ -30,9 +30,33 @@ func bridgedGatewayNodeSetup(nodeName, bridgeInterface string) (string, string, 
 	}
 
 	// ovn-bridge-mappings maps a physical network name to a local ovs bridge
-	// that provides connectivity to that network.
+	// that provides connectivity to that network. It is in the form of physnet1:br1,physnet2:br2.
+	// Note that there may be multiple ovs bridge mappings, be sure not to override
+	// the mappings for the other physical network
+	stdout, stderr, err = util.RunOVSVsctl("--if-exists", "get", "Open_vSwitch", ".",
+		"external_ids:ovn-bridge-mappings")
+	if err != nil {
+		return "", "", fmt.Errorf("Failed to get ovn-bridge-mappings stderr:%s (%v)", stderr, err)
+	}
+	// skip the existing mapping setting for the specified physicalNetworkName
+	mapString := ""
+	bridgeMappings := strings.Split(stdout, ",")
+	for _, bridgeMapping := range bridgeMappings {
+		m := strings.Split(bridgeMapping, ":")
+		if network := m[0]; network != physicalNetworkName {
+			if len(mapString) != 0 {
+				mapString += ","
+			}
+			mapString += bridgeMapping
+		}
+	}
+	if len(mapString) != 0 {
+		mapString += ","
+	}
+	mapString += physicalNetworkName + ":" + bridgeInterface
+
 	_, stderr, err = util.RunOVSVsctl("set", "Open_vSwitch", ".",
-		fmt.Sprintf("external_ids:ovn-bridge-mappings=%s:%s", util.PhysicalNetworkName, bridgeInterface))
+		fmt.Sprintf("external_ids:ovn-bridge-mappings=%s", mapString))
 	if err != nil {
 		return "", "", fmt.Errorf("Failed to set ovn-bridge-mappings for ovs bridge %s"+
 			", stderr:%s (%v)", bridgeInterface, stderr, err)
@@ -126,11 +150,15 @@ func CleanupClusterNode(name string, kube *kube.Kube) error {
 
 	switch config.Gateway.Mode {
 	case config.GatewayModeLocal:
-		err = cleanupLocalnetGateway()
+		err = cleanupLocalnetGateway(util.PhysicalNetworkName)
 	case config.GatewayModeSpare:
 		nodeName := strings.ToLower(node.Name)
 		err = cleanupSpareGateway(config.Gateway.Interface, nodeName)
 	case config.GatewayModeShared:
+		err = cleanupLocalnetGateway(util.LocalNetworkName)
+		if err != nil {
+			logrus.Errorf("Failed to cleanup Localnet Gateway, error: %v", err)
+		}
 		err = cleanupSharedGateway()
 	}
 	if err != nil {
@@ -141,6 +169,12 @@ func CleanupClusterNode(name string, kube *kube.Kube) error {
 	stdout, stderr, err := util.RunOVSVsctl("--", "--if-exists", "del-br", "br-int")
 	if err != nil {
 		logrus.Errorf("Failed to delete bridge br-int, stdout: %q, stderr: %q, error: %v", stdout, stderr, err)
+	}
+
+	stdout, stderr, err = util.RunOVSVsctl("--", "--if-exists", "remove", "Open_vSwitch", ".", "external_ids",
+		"ovn-bridge-mappings")
+	if err != nil {
+		logrus.Errorf("Failed to delete ovn-bridge-mappings, stdout: %q, stderr: %q, error: %v", stdout, stderr, err)
 	}
 
 	return nil

go-controller/pkg/cluster/gateway_init_linux_test.go
@@ -39,8 +39,82 @@ func addNodeportLBs(fexec *ovntest.FakeExec, nodeName, tcpLBUUID, udpLBUUID stri
 		Output: udpLBUUID,
 	})
 	fexec.AddFakeCmdsNoOutputNoError([]string{
-		"ovn-nbctl --timeout=15 set logical_router GR_node1 load_balancer=" + tcpLBUUID,
-		"ovn-nbctl --timeout=15 add logical_router GR_node1 load_balancer " + udpLBUUID,
+		"ovn-nbctl --timeout=15 set logical_router GR_" + nodeName + " load_balancer=" + tcpLBUUID,
+		"ovn-nbctl --timeout=15 add logical_router GR_" + nodeName + " load_balancer " + udpLBUUID,
+	})
+}
+
+func initLocalOnlyGatewayTest(fexec *ovntest.FakeExec, nodeName, clusterCIDR, clusterRouterUUID,
+	systemID, lrpMAC, lrpIP string) {
+	const (
+		brLocalnetMAC string = "11:22:33:44:55:66"
+	)
+	gwRouter := "GR_local_" + nodeName
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovs-vsctl --timeout=15 --may-exist add-br br-local",
+		"ip link set br-local up",
+		"ovs-vsctl --timeout=15 --may-exist add-port br-local br-nexthop -- set interface br-nexthop type=internal",
+		"ip link set br-nexthop up",
+		"ip addr flush dev br-nexthop",
+		"ip addr add 169.254.33.1/24 dev br-nexthop",
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovs-vsctl --timeout=15 --if-exists get interface br-local mac_in_use",
+		Output: brLocalnetMAC,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovs-vsctl --timeout=15 set bridge br-local other-config:hwaddr=" + brLocalnetMAC,
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovs-vsctl --timeout=15 --if-exists get Open_vSwitch . external_ids:ovn-bridge-mappings",
+		Output: util.PhysicalNetworkName + ":breth0",
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovs-vsctl --timeout=15 set Open_vSwitch . external_ids:ovn-bridge-mappings=" + util.PhysicalNetworkName + ":breth0" + "," + util.LocalNetworkName + ":br-local",
+	})
+
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find logical_router external_ids:k8s-cluster-router=yes",
+		Output: clusterRouterUUID,
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovs-vsctl --timeout=15 --if-exists get Open_vSwitch . external_ids:system-id",
+		Output: systemID,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 -- --may-exist lr-add " + gwRouter + " -- set logical_router " + gwRouter + " options:chassis=" + systemID + " external_ids:physical_ip=169.254.33.2",
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 get logical_switch_port jtor-" + gwRouter + " dynamic_addresses",
+		Output: "",
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --wait=sb --may-exist lsp-add join jtor-" + gwRouter + " -- --if-exists clear logical_switch_port jtor-" + gwRouter + " dynamic_addresses -- lsp-set-addresses jtor-" + gwRouter + " dynamic",
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 get logical_switch_port jtor-" + gwRouter + " dynamic_addresses",
+		Output: lrpMAC + " " + lrpIP,
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --if-exists get logical_switch join other-config:subnet",
+		Output: "\"100.64.0.1/16\"",
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 -- --may-exist lrp-add " + gwRouter + " rtoj-" + gwRouter + " " + lrpMAC + " " + lrpIP + "/16" + " -- set logical_switch_port jtor-" + gwRouter + " type=router options:router-port=rtoj-" + gwRouter + " addresses=router",
+		"ovn-nbctl --timeout=15 --may-exist lr-route-add " + gwRouter + " " + clusterCIDR + " 100.64.0.1",
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --may-exist lr-route-add " + clusterRouterUUID + " " + nodeName + "/32 " + lrpIP,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --may-exist ls-add ext_local_" + nodeName,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 -- --may-exist lsp-add ext_local_" + nodeName + " br-local_" + nodeName + " -- lsp-set-addresses br-local_" + nodeName + " unknown -- lsp-set-type br-local_" + nodeName + " localnet -- lsp-set-options br-local_" + nodeName + " network_name=" + util.LocalNetworkName,
+		"ovn-nbctl --timeout=15 -- --if-exists lrp-del rtoe-" + gwRouter + " -- lrp-add " + gwRouter + " rtoe-" + gwRouter + " " + brLocalnetMAC + " 169.254.33.2/24 -- set logical_router_port rtoe-" + gwRouter + " external-ids:gateway-physical-ip=yes",
+		"ovn-nbctl --timeout=15 -- --may-exist lsp-add ext_local_" + nodeName + " etor-" + gwRouter + " -- set logical_switch_port etor-" + gwRouter + " type=router options:router-port=rtoe-" + gwRouter + " addresses=\"" + brLocalnetMAC + "\"",
+		"ovn-nbctl --timeout=15 --may-exist lr-route-add " + gwRouter + " 0.0.0.0/0 169.254.33.1 rtoe-" + gwRouter,
+		"ovn-nbctl --timeout=15 --may-exist lr-nat-add " + gwRouter + " snat 169.254.33.2 " + clusterCIDR,
 	})
 }
 
@@ -48,7 +122,7 @@ func shareGatewayInterfaceTest(app *cli.App, testNS ns.NetNS,
 	eth0Name, eth0MAC, eth0IP, eth0GWIP, eth0CIDR string, gatewayVLANID uint) {
 	app.Action = func(ctx *cli.Context) error {
 		const (
-			nodeName          string = "node1"
+			nodeName          string = "10.1.1.15"
 			lrpMAC            string = "00:00:00:05:46:c3"
 			lrpIP             string = "100.64.0.3"
 			lrpCIDR           string = lrpIP + "/16"
@@ -60,8 +134,6 @@ func shareGatewayInterfaceTest(app *cli.App, testNS ns.NetNS,
 			gwRouter          string = "GR_" + nodeName
 			clusterIPNet      string = "10.1.0.0"
 			clusterCIDR       string = clusterIPNet + "/16"
-			mgtPortName       string = "k8s-" + nodeName
-			mgtPortIP         string = "10.1.1.2"
 		)
 
 		fexec := ovntest.NewFakeExec()
@@ -98,6 +170,12 @@ func shareGatewayInterfaceTest(app *cli.App, testNS ns.NetNS,
 		})
 		fexec.AddFakeCmdsNoOutputNoError([]string{
 			"ovs-vsctl --timeout=15 set bridge breth0 other-config:hwaddr=" + eth0MAC,
+		})
+		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+			Cmd:    "ovs-vsctl --timeout=15 --if-exists get Open_vSwitch . external_ids:ovn-bridge-mappings",
+			Output: "",
+		})
+		fexec.AddFakeCmdsNoOutputNoError([]string{
 			"ovs-vsctl --timeout=15 set Open_vSwitch . external_ids:ovn-bridge-mappings=" + util.PhysicalNetworkName + ":breth0",
 		})
 		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
@@ -156,37 +234,9 @@ GR_openshift-master-node chassis=6a47b33b-89d3-4d65-ac31-b19b549326c7 lb_force_s
 			"ovn-nbctl --timeout=15 --may-exist lr-route-add " + clusterRouterUUID + " " + lrpIP + " " + lrpIP,
 			"ovn-nbctl --timeout=15 --may-exist --policy=src-ip lr-route-add " + clusterRouterUUID + " " + nodeSubnet + " " + lrpIP,
 		})
+		initLocalOnlyGatewayTest(fexec, nodeName, clusterCIDR, clusterRouterUUID, systemID, lrpMAC, lrpIP)
 		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
-			Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find logical_router external_ids:k8s-cluster-router=yes",
-			Output: clusterRouterUUID,
-			Action: func() error {
-				defer GinkgoRecover()
-
-				err := netlink.LinkAdd(&netlink.Dummy{
-					LinkAttrs: netlink.LinkAttrs{
-						Name: mgtPortName,
-					},
-				})
-				Expect(err).NotTo(HaveOccurred())
-				l, err := netlink.LinkByName(mgtPortName)
-				Expect(err).NotTo(HaveOccurred())
-				err = netlink.LinkSetUp(l)
-				Expect(err).NotTo(HaveOccurred())
-
-				// Add an IP address
-				addr, err := netlink.ParseAddr(mgtPortIP + "/24")
-				Expect(err).NotTo(HaveOccurred())
-				err = netlink.AddrAdd(l, addr)
-				Expect(err).NotTo(HaveOccurred())
-
-				return nil
-			},
-		})
-		fexec.AddFakeCmdsNoOutputNoError([]string{
-			"ovn-nbctl --timeout=15 --may-exist lr-route-add " + clusterRouterUUID + " " + eth0IP + "/32 " + mgtPortIP,
-		})
-		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
-			Cmd:    "ovs-vsctl --timeout=15 wait-until Interface patch-breth0_node1-to-br-int ofport>0 -- get Interface patch-breth0_node1-to-br-int ofport",
+			Cmd:    "ovs-vsctl --timeout=15 wait-until Interface patch-breth0_" + nodeName + "-to-br-int ofport>0 -- get Interface patch-breth0_" + nodeName + "-to-br-int ofport",
 			Output: "5",
 		})
 		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
@@ -202,7 +252,7 @@ GR_openshift-master-node chassis=6a47b33b-89d3-4d65-ac31-b19b549326c7 lb_force_s
 		})
 		// nodePortWatcher()
 		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
-			Cmd:    "ovs-vsctl --timeout=15 --if-exists get interface patch-breth0_node1-to-br-int ofport",
+			Cmd:    "ovs-vsctl --timeout=15 --if-exists get interface patch-breth0_" + nodeName + "-to-br-int ofport",
 			Output: "9",
 		})
 		fexec.AddFakeCmd(&ovntest.ExpectedCmd{
@@ -498,6 +548,12 @@ var _ = Describe("Gateway Init Operations", func() {
 			})
 			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovs-vsctl --timeout=15 set bridge br-local other-config:hwaddr=" + brLocalnetMAC,
+			})
+			fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+				Cmd:    "ovs-vsctl --timeout=15 --if-exists get Open_vSwitch . external_ids:ovn-bridge-mappings",
+				Output: "",
+			})
+			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovs-vsctl --timeout=15 set Open_vSwitch . external_ids:ovn-bridge-mappings=" + util.PhysicalNetworkName + ":br-local",
 			})
 			fexec.AddFakeCmd(&ovntest.ExpectedCmd{

go-controller/pkg/cluster/gateway_localnet.go
@@ -157,13 +157,13 @@ func initLocalnetGateway(nodeName string, clusterIPSubnet []string,
 			localnetBridgeNextHop, err)
 	}
 
-	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, localnetBridgeName)
+	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, localnetBridgeName, util.PhysicalNetworkName)
 	if err != nil {
 		return fmt.Errorf("failed to set up shared interface gateway: %v", err)
 	}
 
 	err = util.GatewayInit(clusterIPSubnet, nodeName, ifaceID, localnetGatewayIP,
-		macAddress, localnetGatewayNextHop, subnet, true, nil)
+		macAddress, localnetGatewayNextHop, subnet, util.PhysicalNetworkName, nil)
 	if err != nil {
 		return fmt.Errorf("failed to localnet gateway: %v", err)
 	}
@@ -294,17 +294,25 @@ func localnetNodePortWatcher(ipt util.IPTablesHelper, wf *factory.WatchFactory) 
 	return err
 }
 
-func cleanupLocalnetGateway() error {
+func cleanupLocalnetGateway(physnet string) error {
 	// get bridgeName from ovn-bridge-mappings.
 	stdout, stderr, err := util.RunOVSVsctl("--if-exists", "get", "Open_vSwitch", ".",
 		"external_ids:ovn-bridge-mappings")
 	if err != nil {
 		return fmt.Errorf("Failed to get ovn-bridge-mappings stderr:%s (%v)", stderr, err)
 	}
-	bridgeName := strings.Split(stdout, ":")[1]
-	_, stderr, err = util.RunOVSVsctl("--", "--if-exists", "del-br", bridgeName)
-	if err != nil {
-		return fmt.Errorf("Failed to ovs-vsctl del-br %s stderr:%s (%v)", bridgeName, stderr, err)
+	bridgeMappings := strings.Split(stdout, ",")
+	for _, bridgeMapping := range bridgeMappings {
+		m := strings.Split(bridgeMapping, ":")
+		if physnet == m[0] {
+			bridgeName := m[1]
+			_, stderr, err = util.RunOVSVsctl("--", "--if-exists", "del-br", bridgeName)
+			if err != nil {
+				return fmt.Errorf("Failed to ovs-vsctl del-br %s stderr:%s (%v)", bridgeName, stderr, err)
+			}
+			break
+		}
 	}
-	return err
+
+	return nil
 }

go-controller/pkg/cluster/gateway_localnet_windows.go
@@ -13,7 +13,7 @@ func initLocalnetGateway(nodeName string, clusterIPSubnet []string,
 	return fmt.Errorf("Not implemented yet on Windows")
 }
 
-func cleanupLocalnetGateway() error {
+func cleanupLocalnetGateway(physnet string) error {
 	// TODO: Implement this
 	return fmt.Errorf("Not implemented yet on Windows")
 }

go-controller/pkg/cluster/gateway_shared_intf.go
@@ -253,28 +253,6 @@ func addDefaultConntrackRules(nodeName, gwBridge, gwIntf string) error {
 	return nil
 }
 
-func addStaticRouteToHost(nodeName, nicIP string) error {
-	k8sClusterRouter, err := util.GetK8sClusterRouter()
-	if err != nil {
-		return err
-	}
-
-	k8sMgmtIntfName := util.GetK8sMgmtIntfName(nodeName)
-	k8sMgmtIntfIPAddress, err := getIPv4Address(k8sMgmtIntfName)
-	if err != nil {
-		return fmt.Errorf("failed to get interface IP address for %s (%v)",
-			k8sMgmtIntfName, err)
-	}
-	prefix := strings.Split(nicIP, "/")[0] + "/32"
-	nexthop := strings.Split(k8sMgmtIntfIPAddress, "/")[0]
-	_, stderr, err := util.RunOVNNbctl("--may-exist", "lr-route-add", k8sClusterRouter, prefix, nexthop)
-	if err != nil {
-		return fmt.Errorf("failed to add static route '%s via %s' for host %q on %s "+
-			"stderr: %q, error: %v", nicIP, k8sMgmtIntfIPAddress, nodeName, k8sClusterRouter, stderr, err)
-	}
-	return nil
-}
-
 func initSharedGateway(
 	nodeName string, clusterIPSubnet []string, subnet,
 	gwNextHop, gwIntf string, wf *factory.WatchFactory) error {
@@ -309,7 +287,7 @@ func initSharedGateway(
 		return fmt.Errorf("%s does not have a ipv4 address", bridgeName)
 	}
 
-	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, bridgeName)
+	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, bridgeName, util.PhysicalNetworkName)
 	if err != nil {
 		return fmt.Errorf("failed to set up shared interface gateway: %v", err)
 	}
@@ -321,21 +299,18 @@ func initSharedGateway(
 	}
 
 	err = util.GatewayInit(clusterIPSubnet, nodeName, ifaceID, ipAddress,
-		macAddress, gwNextHop, subnet, true, lspArgs)
+		macAddress, gwNextHop, subnet, util.PhysicalNetworkName, lspArgs)
 	if err != nil {
 		return fmt.Errorf("failed to init shared interface gateway: %v", err)
 	}
 
-	// Add static routes to OVN Cluster Router to enable pods on this Node to
-	// reach the host IP
-	err = addStaticRouteToHost(nodeName, ipAddress)
-	if err != nil {
+	if err = initLocalOnlyGateway(nodeName, clusterIPSubnet); err != nil {
 		return err
 	}
 
 	// Program cluster.GatewayIntf to let non-pod traffic to go to host
 	// stack
-	if err := addDefaultConntrackRules(nodeName, bridgeName, gwIntf); err != nil {
+	if err = addDefaultConntrackRules(nodeName, bridgeName, gwIntf); err != nil {
 		return err
 	}
 
@@ -363,5 +338,64 @@ func cleanupSharedGateway() error {
 			return fmt.Errorf("Failed to delete port %s stderr:%s (%v)", port, stderr, err)
 		}
 	}
+	return nil
+}
+
+func initLocalOnlyGateway(nodeName string, clusterIPSubnet []string) error {
+	// Create a localnet OVS bridge.
+	localnetBridgeName := "br-local"
+	_, stderr, err := util.RunOVSVsctl("--may-exist", "add-br",
+		localnetBridgeName)
+	if err != nil {
+		return fmt.Errorf("Failed to create localnet bridge %s"+
+			", stderr:%s (%v)", localnetBridgeName, stderr, err)
+	}
+
+	_, _, err = util.RunIP("link", "set", localnetBridgeName, "up")
+	if err != nil {
+		return fmt.Errorf("failed to up %s (%v)", localnetBridgeName, err)
+	}
+
+	// Create a localnet bridge nexthop
+	localnetBridgeNextHop := "br-nexthop"
+	_, stderr, err = util.RunOVSVsctl("--may-exist", "add-port",
+		localnetBridgeName, localnetBridgeNextHop, "--", "set",
+		"interface", localnetBridgeNextHop, "type=internal")
+	if err != nil {
+		return fmt.Errorf("Failed to create localnet bridge next hop %s"+
+			", stderr:%s (%v)", localnetBridgeNextHop, stderr, err)
+	}
+	_, _, err = util.RunIP("link", "set", localnetBridgeNextHop, "up")
+	if err != nil {
+		return fmt.Errorf("failed to up %s (%v)", localnetBridgeNextHop, err)
+	}
+
+	// Flush IPv4 address of localnetBridgeNextHop.
+	_, _, err = util.RunIP("addr", "flush", "dev", localnetBridgeNextHop)
+	if err != nil {
+		return fmt.Errorf("failed to flush ip address of %s (%v)",
+			localnetBridgeNextHop, err)
+	}
+
+	// Set localnetBridgeNextHop with an IP address.
+	_, _, err = util.RunIP("addr", "add",
+		localnetGatewayNextHopSubnet,
+		"dev", localnetBridgeNextHop)
+	if err != nil {
+		return fmt.Errorf("failed to assign ip address to %s (%v)",
+			localnetBridgeNextHop, err)
+	}
+
+	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, localnetBridgeName, util.LocalNetworkName)
+	if err != nil {
+		return fmt.Errorf("failed to set up shared interface gateway: %v", err)
+	}
+
+	err = util.LocalGatewayInit(clusterIPSubnet, nodeName, ifaceID, localnetGatewayIP,
+		macAddress, localnetGatewayNextHop, util.LocalNetworkName)
+	if err != nil {
+		return fmt.Errorf("failed to localnet gateway: %v", err)
+	}
+
 	return nil
 }

go-controller/pkg/cluster/gateway_spare_intf.go
@@ -50,7 +50,7 @@ func initSpareGateway(nodeName string, clusterIPSubnet []string,
 	}
 
 	err = util.GatewayInit(clusterIPSubnet, nodeName, ifaceID, ipAddress,
-		macAddress, gwNextHop, subnet, false, nil)
+		macAddress, gwNextHop, subnet, "", nil)
 	if err != nil {
 		return fmt.Errorf("failed to init spare interface gateway: %v", err)
 	}

go-controller/pkg/cluster/master_test.go
@@ -85,6 +85,29 @@ func defaultFakeExec(nodeSubnet, nodeName string) (*ovntest.FakeExec, string, st
 	return fexec, tcpLBUUID, udpLBUUID
 }
 
+func deleteLocalOnlyGatewayTest(fexec *ovntest.FakeExec, nodeName, clusterRouterUUID string) {
+	const (
+		localNodeRouteUUID string = "0cac12cf-3e0f-4682-b028-5ea2e0001962"
+	)
+
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --if-exist get logical_router_port rtoj-GR_local_" + nodeName + " networks",
+		Output: "[\"100.64.0.4/16\"]",
+	})
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find logical_router_static_route nexthop=100.64.0.4",
+		Output: localNodeRouteUUID,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --if-exists remove logical_router " + clusterRouterUUID + " static_routes " + localNodeRouteUUID,
+	})
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --if-exist lsp-del jtor-GR_local_" + nodeName,
+		"ovn-nbctl --timeout=15 --if-exist lr-del GR_local_" + nodeName,
+		"ovn-nbctl --timeout=15 --if-exist ls-del ext_local_" + nodeName,
+	})
+}
+
 var _ = Describe("Master Operations", func() {
 	var app *cli.App
 
@@ -219,7 +242,6 @@ var _ = Describe("Master Operations", func() {
 				node1Name         string = "openshift-node-1"
 				node1Subnet       string = "10.128.0.0/24"
 				node1RouteUUID    string = "0cac12cf-3e0f-4682-b028-5ea2e0001962"
-				node1mgtRouteUUID string = "0cac12cf-3e0f-4682-b028-5ea2e0001963"
 				masterName        string = "openshift-master-node"
 				masterSubnet      string = "10.128.2.0/24"
 				masterGWCIDR      string = "10.128.2.1/24"
@@ -259,19 +281,12 @@ subnet=%s
 			})
 			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovn-nbctl --timeout=15 --if-exists remove logical_router " + clusterRouterUUID + " static_routes " + node1RouteUUID,
-			})
-			fexec.AddFakeCmd(&ovntest.ExpectedCmd{
-				Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find logical_router_static_route nexthop=10.128.0.2",
-				Output: node1mgtRouteUUID,
-			})
-			fexec.AddFakeCmdsNoOutputNoError([]string{
-				"ovn-nbctl --timeout=15 --if-exists remove logical_router " + clusterRouterUUID + " static_routes " + node1mgtRouteUUID,
-			})
-			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovn-nbctl --timeout=15 --if-exist lsp-del jtor-GR_" + node1Name,
 				"ovn-nbctl --timeout=15 --if-exist lr-del GR_" + node1Name,
 				"ovn-nbctl --timeout=15 --if-exist ls-del ext_" + node1Name,
 			})
+
+			deleteLocalOnlyGatewayTest(fexec, node1Name, clusterRouterUUID)
 
 			// Expect the code to re-add the master node (which still exists)
 			// when the factory watch begins and enumerates all existing

commit 25e5c075104074fe9180899ed5e6dcc8bb55a407

remove unnecessary check in deleteNode() function
Fixes: a9cd71c62721 (Cannot access K8s service cluster IP from the
       K8s master)
Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/cluster/master.go
@@ -327,10 +327,8 @@ func (cluster *OvnClusterController) deleteNode(nodeName string, nodeSubnet *net
 		logrus.Errorf("Error deleting node %s logical network: %v", nodeName, err)
 	}
 
-	if nodeSubnet != nil {
-		if err := util.GatewayCleanup(nodeName); err != nil {
-			return fmt.Errorf("Failed to clean up node %s gateway: (%v)", nodeName, err)
-		}
+	if err := util.GatewayCleanup(nodeName); err != nil {
+		return fmt.Errorf("Failed to clean up node %s gateway: (%v)", nodeName, err)
 	}
 
 	return nil